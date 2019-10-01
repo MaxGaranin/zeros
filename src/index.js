0af784716c9475ca2242d1306fa8804c525cbef1
GaraninMS <maxgaranin@gmail.com>
2019-10-01 14:56:46 +0400

    feat: solve task, need some refactoring

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 module.exports = function zeros(expression) {
-  let result = 1;
+  let resultTwoCount = 0;
+  let resultFiveCount = 0;
 
   let currentNumber = '';
   let currentOp = '';
@@ -7,7 +8,7 @@ module.exports = function zeros(expression) {
   for (const ch of expression) {
     if (ch == '!') {
       currentOp += ch;
-      
+
       if (currentOp.length > 2) {
         throw new SyntaxError('More than two "!" in expression');
       }
@@ -26,80 +27,82 @@ module.exports = function zeros(expression) {
   intermediateCalc();
 
   function intermediateCalc() {
-    if (currentNumber.length == 0) {
+    if (currentNumber.length == 0 || currentOp.length == 0) {
       throw new SyntaxError('Wrong expression');
     }
 
     let n = parseInt(currentNumber);
-    let fact = 1;
-    if (currentOp == '') {
-      fact = n;
-    }
-    else if (currentOp == '!') {
-      fact = normalFact(n);
+    let twoCount = 0;
+    let fiveCount = 0;
+
+    if (currentOp == '!') {
+      if (isEven(n)) {
+        let res = getTwosCountFromEvenFact(n);
+        twoCount = res.twoCount;
+        fiveCount = res.fiveCount;
+
+        fiveCount += getFivesCountFromOddFact(n - 1);
+      }
+      else {
+        fiveCount = getFivesCountFromOddFact(n);
+
+        let res = getTwosCountFromEvenFact(n - 1);
+        twoCount = res.twoCount;
+        fiveCount += res.fiveCount;
+      }
     }
     else if (currentOp == "!!") {
       if (isEven(n)) {
-        fact = evenFact(n);
+        let res = getTwosCountFromEvenFact(n);
+        twoCount = res.twoCount;
+        fiveCount = res.fiveCount;
       }
       else {
-        fact = oddFact(n);
+        fiveCount = getFivesCountFromOddFact(n);
       }
     }
-    result *= fact;
+
+    resultTwoCount += twoCount;
+    resultFiveCount += fiveCount;
   }
 
-  let count = findZeros(result);
+  let resultCount = Math.min(resultTwoCount, resultFiveCount);
+  return resultCount;
+}
+
+function getTwosCountFromEvenFact(n) {
+  let twoCount = 0;
+  let fiveCount = 0;
+  
+  for (let i = 2; i <= n; i += 2) {
+    twoCount += getCount(i, 2);
+    fiveCount += getCount(i, 5);
+  }
+
+  return {
+    twoCount: twoCount,
+    fiveCount: fiveCount
+  };
+}
+
+function getFivesCountFromOddFact(n) {
+  let count = 0;
+  for (let i = 3; i <= n; i += 2) {
+    count += getCount(i, 5);
+  }
+
   return count;
 }
 
-function normalFact(n) {
-  if (n == 0 || n == 1) return 1;
-
-  let result = 1;
-  for (let i = 2; i <= n; i++) {
-    result *= i;
+function getCount(n, base) {
+  let count = 0;
+  while (n % base == 0) {
+    count++;
+    n = n / base;
   }
-
-  return result;
-}
-
-function evenFact(n) {
-  if (!isEven(n)) throw new SyntaxError("The number must be even.");
-  if (n == 0 || n == 2) return 2;
-
-  let result = 2;
-  for (let i = 4; i <= n; i += 2) {
-    result *= i;
-  }
-
-  return result;
-}
-
-function oddFact(n) {
-  if (isEven(n)) throw new SyntaxError("The number must be odd.");
-  if (n == 0 || n == 1) return 1;
-
-  let result = 1;
-  for (let i = 3; i <= n; i += 2) {
-    result *= i;
-  }
-
-  return result;
+  return count;
 }
 
 function isEven(n) {
   return n % 2 == 0;
-}
-
-function findZeros(n) {
-  let s = n + '';
-
-  let count = 0;
-  for (let i = s.length - 1; i >= 0; i--) {
-    if (s[i] != '0') break;
-    count++;
-  }
-
-  return count;
 }
